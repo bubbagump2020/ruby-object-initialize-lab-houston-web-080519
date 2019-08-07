9be43d46d7470421776390a29a7a51092eeb92ef
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-07 22:13:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,13 @@
 class Dog
-  def initialize(dog_name)
+  def initialize(dog_name, dog_breed="Mutt")
     @name = dog_name
+    @breed = dog_breed
   end
-  def 
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
 end
